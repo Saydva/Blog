89f47ad549ec2980bb01bec3e81de2050894b656
Saydva <kacurikjozef@gmail.com>
2026-01-04 13:37:12 +0100

    intialized empty react project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="mt-2 text-blue-700 ">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
